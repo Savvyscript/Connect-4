67fd799cfa57193c8851513cec4c2d6fb337d910
savvyscript <nm3142@hotmail.com>
2017-03-08 08:22:21 -0500

    update 1

---
+++ project_1/part3.js
@@ -0,0 +1,7 @@
+// var amount = 5;
+// for (var i = 0; i < amount; i++){
+//     var new_div = document.createElement("div");
+//     new_div.className = "column";
+//     document.body.appendChild(new_div);
+//     console.log("This is repeat " + i);
+// }
